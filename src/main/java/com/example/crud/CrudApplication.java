f9746703b44fb34ace111fcf0478428143a98c03
GuptaSwapnesh <guptaswapnesh85@gmail.com>
2024-10-15 12:22:34 +0530

    Changes made: -> Updated imports & Annotations in java classes -> Updated pom.xml with => Spring Boot Version to 3.3.4 => MySql version to 8.2.0 => Added Jakarta dependency => Added Dependency Check plugin to check VA issues => Added DistributionManagement for release & snapshot jars => Pointed to Nexus repo for artifact upload

--- src/main/java/com/example/crud/CrudApplication.java
+++ src/main/java/com/example/crud/CrudApplication.java
@@ -2,8 +2,15 @@ package com.example.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages =
+		"com.example.crud.repository")
+@ComponentScan(basePackages = { "com.example.crud.*" })
+@EntityScan("com.example.crud.entity")
 public class CrudApplication {
 
 	public static void main(String[] args) {
